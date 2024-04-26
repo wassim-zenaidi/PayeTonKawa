73dfbe8894b8fc0dbe69746c474d3b3acb847a09
@wassimzen44 <wassimzen44@gmail.com>
2024-04-26 10:51:37 +0200

    feat(add): Add Auth0 connexion

--- PayeTonKawa-VueProject/src/App.vue
+++ PayeTonKawa-VueProject/src/App.vue
@@ -1,10 +1,38 @@
-<script setup>
-
-</script>
-
 <template>
- 
+ <div>
+    <div v-if="isAuthenticated">
+      <button @click="logout">Logout</button>
+      <h2>{{ user.name }}</h2>
+      <h2>{{ user.email }}</h2>
+      <h2>{{ user.picture }}</h2>
+    </div>
+    <div v-else>
+      <button @click="login">Login</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import { useAuth0 } from '@auth0/auth0-vue';
+
+
+  export default {
+    setup() {
+      const { loginWithPopup, user, isAuthenticated,logout} = useAuth0();
+
+      return {
+        login: () => {
+          loginWithPopup();
+        },
+        logout: () =>{
+          logout();
+        },
+        user,
+        isAuthenticated
+      }
+    }
+  };
+</script>
 
 <style scoped>
 
--- PayeTonKawa-VueProject/src/main.js
+++ PayeTonKawa-VueProject/src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
-
+import './assets/main.css';
 import { createApp } from 'vue'
+import { createAuth0 } from '@auth0/auth0-vue';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain:"dev-qu8po7wzxi3qc6om.us.auth0.com",
+    clientId: "xWq2klm39umFkOUph3Itl1z7gtb9P88f",
+    authorizationParams: {
+    redirect_uri: window.location.origin
+    }
+  })
+);
+
+app.mount('#app')
+
